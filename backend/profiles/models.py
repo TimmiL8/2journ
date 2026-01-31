3d192a56f3042ac69253ee88d7141d9a365b1911
srgtkrvvv <tr3e1t0ry@gmail.com>
2026-01-31 13:03:18 +0200

    Resolve clashing names issue

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -4,18 +4,17 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    #makes the email field unique so two people can't use the same one
-    email = models.EmailField(unique=True)
-    
-    #tells Django to use the email for logging in
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username'] # Django still needs this internally
-class Profile(models.Model):
-    # This links the Profile to exactly one User
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
     displayed_name = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
 
     def __str__(self):
         return f"Profile for {self.user.email}"
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE, related_name='user')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
