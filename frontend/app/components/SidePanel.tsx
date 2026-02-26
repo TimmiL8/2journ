7b22b4f13062c72e7d4560a7ad9d8ec54b0ec4f9
TimmiL8 <timmi.ua@gmail.com>
2026-02-26 23:51:42 +0200

    Finished sidebar and made icons

--- frontend/app/components/SidePanel.tsx
+++ frontend/app/components/SidePanel.tsx
@@ -1,40 +1,48 @@
-import {JSX} from "react";
-import Image from "next/image";
+"use client"
+
+import React, {JSX} from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import HomeIcon from "@/app/icons/HomeIcon";
+import SearchPlacesIcon from "@/app/icons/SearchPlacesIcon";
+import MyTripsIcon from "@/app/icons/MyTripsIcon";
+import SavedIcon from "@/app/icons/SavedPlacesIcon";
+import ProfileIcon from "@/app/icons/ProfileIcon";
+import LogOutIcon from "@/app/icons/LogOutIcon";
 
 interface NavButtonProps {
     navPath: string;
-    iconPath: string;
+    Icon: React.ElementType;
     title: string;
 }
 
 export default function SidePanel() {
     return (
-        <div className="h-full w-full max-w-[280px] bg-white rounded-2xl justify-between p-9">
-            <div className="flex flex-col items-start gap-3">
-                <NavButton navPath={"/home"} iconPath={"/icons/home-icon.svg"} title={"Home"} />
-                <NavButton navPath={"/search-place"} iconPath={"/icons/Search-place-icon.svg"} title={"Search place"} />
-                <NavButton navPath={"/my-trips"} iconPath={"/icons/My-trips-icon.svg"} title={"My trips"} />
-                <NavButton navPath={"/favourites"} iconPath={"/icons/Favourites-icon.svg"} title={"Favourites"} />
-                <NavButton navPath={"/profile"} iconPath={"/icons/Profile-icon.svg"} title={"Profile"} />
+        <div className="h-full w-full max-w-[280px] bg-white rounded-2xl flex flex-col justify-between p-6">
+            <div className="flex flex-col items-start gap-2">
+                <NavButton navPath={"/"} Icon={HomeIcon} title={"Home"} />
+                <NavButton navPath={"/search-place"} Icon={SearchPlacesIcon} title={"Search place"} />
+                <NavButton navPath={"/my-trips"} Icon={MyTripsIcon} title={"My trips"} />
+                <NavButton navPath={"/saved-places"} Icon={SavedIcon} title={"Saved Places"} />
+                <NavButton navPath={"/profile"} Icon={ProfileIcon} title={"Profile"} />
             </div>
-            <div>
-                <p>Log out</p>
-            </div>
+            <NavButton navPath={"/LogOut"} Icon={LogOutIcon} title={"Log out"} />
         </div>
     )
 }
 
-function NavButton({navPath, iconPath, title} : NavButtonProps) : JSX.Element {
+function NavButton({ navPath, Icon, title }: NavButtonProps): JSX.Element {
+    const pathname : string = usePathname();
+    const isActive: boolean = pathname === navPath;
+
+    const styleIfOnPage = isActive
+        ? "text-white bg-primary"
+        : "text-black hover:bg-gray-50 transition-colors";
+
     return (
-        <Link href={navPath} passHref className={"flex flex-row  items-center w-full color-background gap-4.5"}>
-            <Image
-                src={iconPath}
-                width={32}
-                height={32}
-                alt={title}
-            />
-            <p className={"text-xl font-semibold"}>{title}</p>
+        <Link href={navPath} className={`flex flex-row h-13 items-center w-full gap-4.5 rounded-lg px-4 ${styleIfOnPage}`}>
+            <Icon className={""}/>
+            <p className="text-xl font-semibold">{title}</p>
         </Link>
-    )
+    );
 }
---
+++ frontend/app/icons/SavedPlacesIcon.tsx
@@ -0,0 +1,13 @@
+export default function HomeIcon({ className }: { className?: string }) {
+    return (
+        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+            <path d="M9.99935 5.33338C5.94935 5.33338 2.66602 8.61671 2.66602 12.6667C2.66602 20 11.3327 26.6667 15.9993 28.2174C20.666 26.6667 29.3327 20 29.3327 12.6667C29.3327 8.61671 26.0493 5.33338 21.9993 5.33338C19.5193 5.33338 17.326 6.56471 15.9993 8.44938C15.323 7.4863 14.4246 6.70031 13.3803 6.15793C12.3359 5.61554 11.1762 5.33271 9.99935 5.33338Z"
+                  stroke="currentColor"
+                  strokeWidth="2.66667"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+        </svg>
+
+
+    );
+}
