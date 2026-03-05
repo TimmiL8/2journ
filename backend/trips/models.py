9e9859cab1857badae859fa490a45cf04683df20
srgtkrvvv <tr3e1t0ry@gmail.com>
2026-03-05 21:02:30 +0200

    Updated trips

--- backend/trips/models.py
+++ backend/trips/models.py
@@ -1,18 +1,29 @@
+from datetime import date
+
 from django.db import models
 from uuid import uuid4
+from places.models import Place
 # Create your models here.
 class Trip(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)   
-    title = models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200, default='Trip')
     description = models.TextField(null=True, blank=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(default=date.today)
+    places = models.ManyToManyField(Place)
 
-    # Fields to be added later
-    # places = models.ManyToManyField('places.Place', related_name='trips')
     # owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='trips')
-    # participants = models.ManyToManyField('auth.User', related_name='participating_trips', blank=True)
+
 
     def __str__(self):
         return f"{self.title} {self.start_date} - {self.end_date} by \
         {self.owner.username if hasattr(self, 'owner') else 'Unknown'}"
+
+
+import uuid
+
+
+
+
+
+
